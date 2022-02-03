368313ffd53f89eae95faf12d4af23ad423b3226
Shantanu Pawar <66003064+pawar41@users.noreply.github.com>
2022-02-03 23:36:29 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -30,3 +30,5 @@ sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
 sudo apt install atom -y
+
+sudo apt-get install python3-tk python3-dev -y

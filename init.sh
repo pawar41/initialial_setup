44ddf2bbaf59ffb585424ebf2ad9ba640341b2d7
pawar41 <shantanup410@gmail.com>
2022-06-01 19:56:39 +0530

    added jekyll installation

--- init.sh
+++ init.sh
@@ -97,3 +97,11 @@ pip3 install --user --upgrade https://github.com/pdfarranger/pdfarranger/zipball
 
 
 #sudo apt-get install  -y
+sudo apt-get install ruby-full build-essential zlib1g-dev -y
+
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+sudo gem install jekyll bundler
